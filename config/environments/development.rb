2aa938e438b8271ef42163c37ccecd3322b46243
冯文军 <fengwenjun@zeju.com>
2018-03-09 12:31:34 +0800

    xxx

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { Setting.domain }
+  config.action_mailer.default_url_options = { host: Setting.domain }
 
   # mailer
   config.action_mailer.delivery_method = :smtp
